0407d2b7cfd74e23898c97d7af85fa51db49663c
joe <zhangyj@3gmac.com>
2015-07-03 00:56:50 +0800

    transition

--- app/src/main/java/demo/joez/com/item/TransitionsFragment.java
+++ app/src/main/java/demo/joez/com/item/TransitionsFragment.java
@@ -6,6 +6,7 @@ import android.transition.ChangeBounds;
 import android.transition.Fade;
 import android.transition.Scene;
 import android.transition.Transition;
+import android.transition.TransitionInflater;
 import android.transition.TransitionManager;
 import android.transition.TransitionSet;
 import android.view.LayoutInflater;
@@ -19,8 +20,9 @@ import demo.joez.com.demolist.R;
  * Created by JoeZ on 2015/7/2.
  */
 public class TransitionsFragment extends  BaseFragment {
-    private Scene mSceneFirst,mSceneSecond;
+    private Scene mSceneFirst,mSceneSecond,mSceneThird;
     private boolean mIsSecond;
+    private int mFlag=1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_transitions,container,false);
@@ -28,9 +30,22 @@ public class TransitionsFragment extends  BaseFragment {
         tvControll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Transition fadeTransition = new Fade();
-                TransitionSet set = new TransitionSet().addTransition(new AutoTransition());
-                TransitionManager.go(mIsSecond?mSceneFirst:mSceneSecond,set);
+                mFlag++;
+                Scene scene=null;
+                if(mFlag == 3){
+                    scene = mSceneThird;
+                }else if(mFlag == 2){
+                    scene = mSceneSecond;
+                }else{
+                    mFlag = 1;
+                    scene = mSceneFirst;
+                }
+//                Transition fadeTransition = new Fade();
+//                Transition fadeTransition = TransitionInflater.from(getActivity()).inflateTransition(R.transition.fade_transition);
+                TransitionSet set = new TransitionSet().addTransition(new Fade());
+//                TransitionSet set = (TransitionSet) TransitionInflater.from(getActivity()).inflateTransition(R.transition.set_transition);
+                TransitionManager.go(scene,set);
+                TransitionManager m=null;;
                 mIsSecond=!mIsSecond;
             }
         });
@@ -38,6 +53,7 @@ public class TransitionsFragment extends  BaseFragment {
         ViewGroup scene_root = (ViewGroup)rootView.findViewById(R.id.scene_root);
         mSceneFirst = Scene.getSceneForLayout(scene_root,R.layout.scene1,getActivity());
         mSceneSecond = Scene.getSceneForLayout(scene_root,R.layout.scene2,getActivity());
+        mSceneThird = new Scene(scene_root,inflater.inflate(R.layout.scene3,null));
         return rootView;
     }
 }
